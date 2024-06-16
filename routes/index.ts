98b61eb17050f8f86da4ef1281ba1c28e7076268
Laura Jane Antunes <laura.antunes@fatec.sp.gov.br>
2024-06-16 18:35:49 -0300

    Delete routes directory- ts versao

--- routes/index.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { authRoutes } from './auth';
-import { clinicRoutes } from './clinics';
-
-export const routes = express.Router()
-
-routes.use('/auth', authRoutes)
-
-routes.use('/clinics', clinicRoutes)
