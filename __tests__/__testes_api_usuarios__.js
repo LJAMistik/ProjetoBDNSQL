01e5d94129c033baff2f74b80caa9bc94f7cbcea
Laura Jane Antunes <laura.antunes@fatec.sp.gov.br>
2024-06-16 21:10:39 -0300

    alt testes

--- __tests__/__testes_api_usuarios__.js
+++ __tests__/__testes_api_usuarios__.js
@@ -1,70 +1,68 @@
-/*
-* Testes na API - Usuários
-* Tecnologias utilizadas:
-* Supertest: Biblioteca para testes na API Rest do NodeJS
-* dotenv: Biblioteca para gerenciar variáveis de ambiente
-*/
-const request = require('supertest')
-const dotenv = require('dotenv')
-dotenv.config() //carrega as variáveis do .env
+const request = require('supertest');
+const dotenv = require('dotenv');
+dotenv.config(); // carrega as variáveis do .env
 
-const baseURL = 'http://localhost:4000/api'
+const baseURL = 'http://localhost:4000/api';
 
 describe('Testes de Registro de Usuario', () => {
-    let token
-    it('Faz o cadastro de um novo usuario no banco', async () => {
-        const senha = process.env.SENHA_USUARIO
-        const response = await request(baseURL)
-        .post('/usuarios')
-        .set('Content-Type', 'application/json')
-        .send({nome:"Yara", email:"yara@teste.com", senha: senha})
-        .expect(201) // Created
-    })
-
-    it('Faz Login e gera token jwt', async () => {
-        const senha = process.env.SENHA_USUARIO
-        const reponse = await request(baseURL)
-        .post('/usuarios/login')
-        .set('Content-Type', 'application/json')
-        .send({email:"andre_silva@gmail.com.br", senha: senha})
-        .expect(200); //OK
-
-        token = reponse.body.access_token
-        expect(token).toBeDefined() //verificação de recebimento de token
-    })
+  let token;
+  it('Faz o cadastro de um novo usuario no banco', async () => {
+      const senha = process.env.SENHA_USUARIO;
+      const response = await request(baseURL)
+          .post('/usuarios')
+          .set('Content-Type', 'application/json')
+          .send({ nome: "Yara", email: "yara@teste.com", senha: senha })
+          .expect(201); // Created
+  });
 
     it('Verificação de email duplicado', async () => {
-        const senha = process.env.SENHA_USUARIO
+        const senha = 'Senh@Forte_2024';
         await request(baseURL)
-          .post('/usuarios')
-          .set('Content-Type','application/json')
-          .send({ email: 'test@example.com', nome: 'Teste', senha: senha });
-    
-        const response = await request(baseURL)
-          .post('/usuarios')
-          .set('Content-Type','application/json')
-          .send({ email: 'test@example.com', nome: 'Outro Teste', senha: senha });
-          
-        expect(response.body.errors[0].msg).toBe(`o email já existe!`)
-    })
+            .post('/usuarios')
+            .set('Content-Type', 'application/json')
+            .send({ email: 'test@example.com', nome: 'Teste', senha: senha });
 
-    // it('Armazenamento seguro de senha', async () => {
-    //     const senha = 'senhaForte123!';
-    //     const email = 'secure@example.com';
-    //     const nome = 'Usuário Seguro';
-    
-    //     await request(baseURL)
-    //       .post('/usuarios')
-    //       .set('Content-Type', 'application/json')
-    //       .send({ email, nome, senha });
-    
-    //     const user = await db.collection('usuarios').findOne({ email });
-    
-    //     expect(user).not.toBeNull();
-    //     expect(user.nome).toBe(nome);
-    //     expect(user.email).toBe(email);
-    
-    //     const isPasswordMatch = await bcrypt.compare(senha, user.senha);
-    //     expect(isPasswordMatch).toBe(true);
-    // });
-})
+        const response = await request(baseURL)
+            .post('/usuarios')
+            .set('Content-Type', 'application/json')
+            .send({ email: 'test@example.com', nome: 'Outro Teste', senha: senha });
+
+        expect(response.body.errors[0].msg).toBe('o email já existe!');
+    });
+
+    it('Verificação de senha fraca (sem caracteres especiais)', async () => {
+        const response = await request(baseURL)
+            .post('/usuarios')
+            .set('Content-Type', 'application/json')
+            .send({ nome: "Test", email: "weak@teste.com", senha: "Weak123" });
+
+        expect(response.body.errors[0].msg).toBe('A senha não é segura. Informe no mínimo 1 caractere maiúsculo, 1 minúsculo, 1 número e 1 caractere especial');
+    });
+
+    it('Verificação de senha fraca (sem números)', async () => {
+        const response = await request(baseURL)
+            .post('/usuarios')
+            .set('Content-Type', 'application/json')
+            .send({ nome: "Test", email: "weak@teste.com", senha: "SenhaForte!" });
+
+        expect(response.body.errors[0].msg).toBe('A senha não é segura. Informe no mínimo 1 caractere maiúsculo, 1 minúsculo, 1 número e 1 caractere especial');
+    });
+
+    it('Verificação de senha fraca (sem letras maiúsculas)', async () => {
+        const response = await request(baseURL)
+            .post('/usuarios')
+            .set('Content-Type', 'application/json')
+            .send({ nome: "Test", email: "weak@teste.com", senha: "senha123!" });
+
+        expect(response.body.errors[0].msg).toBe('A senha não é segura. Informe no mínimo 1 caractere maiúsculo, 1 minúsculo, 1 número e 1 caractere especial');
+    });
+
+    it('Verificação de senha fraca (sem letras minúsculas)', async () => {
+        const response = await request(baseURL)
+            .post('/usuarios')
+            .set('Content-Type', 'application/json')
+            .send({ nome: "Test", email: "weak@teste.com", senha: "SENHA123!" });
+
+        expect(response.body.errors[0].msg).toBe('A senha não é segura. Informe no mínimo 1 caractere maiúsculo, 1 minúsculo, 1 número e 1 caractere especial');
+    });
+});
